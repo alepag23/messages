5522a6537b910923ff3ae12d141b593104fbb6d4
alepag <alessandro2ap3@gmail.com>
2026-03-24 12:45:29 +0100

    feat: add repository and service messages

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,21 +1,38 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Param, Post } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 
 @Controller("messages")
 export class MessagesController {
+    messagesService: MessagesService;
+
+    constructor() {
+        // DONT DO THIS ON REAL APPS
+        // USE DEPENDENCY INJECTION
+        this.messagesService = new MessagesService;
+    }
+
     @Get()
     getAllMessages() {
-        return ""
+        return this.messagesService.findAll();
     }
 
     @Get("/:id")
-    getMessageById(@Param("id") id: string) {
+    async getMessageById(@Param("id") id: string) {
         console.log("id: ", id)
+        const message = await this.messagesService.findOne(id);
+
+        if (!message) {
+            throw new NotFoundException("message not found");
+        }
+
+        message
     }
 
     @Post()
     createMessage(@Body() body: CreateMessageDto) {
         console.log("body: ", body);
+        return this.messagesService.create(body.content);
     }
 }
