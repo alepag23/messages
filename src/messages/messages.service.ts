e204ed47e89971c2d1bcae437de863cfa9556303
alepag <alessandro2ap3@gmail.com>
2026-03-24 15:33:39 +0100

    fix: change dependency injection

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,11 +1,10 @@
+import { Injectable } from "@nestjs/common";
 import { MessagesRepository } from "./messages.repository";
 
+@Injectable()
 export class MessagesService {
-    messagesRepo: MessagesRepository;
-    constructor() {
-        // Services is creating its own dependencies
-        //DONT DO THIS IN REAL APPS
-        this.messagesRepo = new MessagesRepository();
+
+    constructor(public messagesRepo: MessagesRepository) {
     }
 
     findOne(id: string) {
